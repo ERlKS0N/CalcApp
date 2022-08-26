16310bf6f13cca17365e35912702cb4f2d6ddee4
ERlKS0N <90794114+ERlKS0N@users.noreply.github.com>
2022-08-25 21:49:15 -0500

    commit push
    It won't crash now hip hip hurray finished try catch.

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -19,46 +19,75 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.numb1ET);
         EditText number2ET = findViewById(R.id.numb2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            if (number1ET.getText().toString().equals("") || (number2ET.getText().toString().equals(""))){
+                numberSumTV.setText("Try Again");
+            }
+            double num1 = Double.parseDouble(number1ET.getText().toString());
+            double num2 = Double.parseDouble(number2ET.getText().toString());
+            double sum = num1 + num2;
 
-        double num1 = Double.parseDouble(number1ET.getText().toString());
-        double num2 = Double.parseDouble(number2ET.getText().toString());
-        double sum = num1 + num2;
+            numberSumTV.setText("" + sum);
+        }catch (Exception e){
+            numberSumTV.setText("Try Again");
+        }
 
-        numberSumTV.setText("" + sum);
     }
     public void findDiff(View view) {
         EditText number1ET = findViewById(R.id.numb1ET);
         EditText number2ET = findViewById(R.id.numb2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            if (number1ET.getText().toString().equals("") || (number2ET.getText().toString().equals(""))){
+                numberSumTV.setText("Try Again");
+            }
+            double num1 = Double.parseDouble(number1ET.getText().toString());
+            double num2 = Double.parseDouble(number2ET.getText().toString());
+            double sum = num1 - num2;
 
-        double num1 = Double.parseDouble(number1ET.getText().toString());
-        double num2 = Double.parseDouble(number2ET.getText().toString());
-        double sum = num1 - num2;
+            numberSumTV.setText("" + sum);
+        }catch (Exception e){
+            numberSumTV.setText("Try Again");
+        }
 
-        numberSumTV.setText("" + sum);
     }
     public void findProduct(View view) {
         EditText number1ET = findViewById(R.id.numb1ET);
         EditText number2ET = findViewById(R.id.numb2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            if (number1ET.getText().toString().equals("") || (number2ET.getText().toString().equals(""))){
+                numberSumTV.setText("Try Again");
+            }
+            double num1 = Double.parseDouble(number1ET.getText().toString());
+            double num2 = Double.parseDouble(number2ET.getText().toString());
+            double sum = num1 * num2;
 
-        double num1 = Double.parseDouble(number1ET.getText().toString());
-        double num2 = Double.parseDouble(number2ET.getText().toString());
-        double sum = num1 * num2;
+            numberSumTV.setText("" + sum);
+        }catch (Exception e){
+            numberSumTV.setText("Try Again");
+        }
 
-        numberSumTV.setText("" + sum);
     }
-    public void findDivison(View view) {
+    public void findDivision(View view) {
         EditText number1ET = findViewById(R.id.numb1ET);
         EditText number2ET = findViewById(R.id.numb2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            if (number1ET.getText().toString().equals("") || (number2ET.getText().toString().equals(""))){
+                numberSumTV.setText("Try Again");
+            }
+            double num1 = Double.parseDouble(number1ET.getText().toString());
+            double num2 = Double.parseDouble(number2ET.getText().toString());
+            double sum = num1 / num2;
 
-        double num1 = Double.parseDouble(number1ET.getText().toString());
-        double num2 = Double.parseDouble(number2ET.getText().toString());
-        double sum = num1 / num2;
+            numberSumTV.setText("" + sum);
+        }catch (Exception e){
+            numberSumTV.setText("Try Again");
+        }
 
-        numberSumTV.setText("" + sum);
     }
+
     public void switchScreens(View view) {
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
